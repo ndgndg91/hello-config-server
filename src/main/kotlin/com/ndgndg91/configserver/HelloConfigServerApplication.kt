a2380d20f8ca378c88462613aba70e6b86a0ebfb
giri.nam <giri.nam@coinone.com>
2024-04-30 18:47:07 +0900

    <feat> config public github config

--- src/main/kotlin/com/ndgndg91/configserver/HelloConfigServerApplication.kt
+++ src/main/kotlin/com/ndgndg91/configserver/HelloConfigServerApplication.kt
@@ -2,8 +2,10 @@ package com.ndgndg91.configserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
 
 @SpringBootApplication
+@EnableConfigServer
 class HelloConfigServerApplication
 
 fun main(args: Array<String>) {
